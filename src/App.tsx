1483915a9682d6305deb0c4276342a57248f0648
Nina Nuss <maximnuss120697@gmail.com>
2026-06-24 15:13:34 +0200

    b

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,13 @@
 import "./App.css";
 import { useState } from "react";
-import { useRef } from 'react';
+import { useRef } from "react";
 
 function App() {
-  const hiddenBlocks = useRef(0)
-  const [isHidden, hideBlock] = useState(true)
-  const [isHiddenTxt, hideText] = useState(false)
+  const hiddenBlocks = useRef(0);
+  const [isHidden, hideBlock] = useState(true);
+  const [isHiddenTxt, hideText] = useState(false);
   const newList = [];
   const checkedIds = new Set();
-
 
   const [list, addVal] = useState([
     {
@@ -19,6 +18,7 @@ function App() {
   ]);
 
   const deleteList = () => {
+    
     const idsToDelete = new Set(checkedIds);
     const newList = list.filter((item) => !idsToDelete.has(item.id));
     // list.forEach((obj) => {
@@ -26,29 +26,32 @@ function App() {
     //     newList.push(obj);
     //   }
     // });
+    
 
-    console.log(newList);
+    console.log("checkedid: ", checkedIds)
     addVal(newList);
+    console.log(newList);
+    console.log(list)
   };
 
   const changeItem = (id) => {
-    if(isHidden){
-      hideBlock(false)
-      hideText(true)
-    }else{
-      hideBlock(true)
-      hideText(false)
+    if (isHidden) {
+      hideBlock(false);
+      hideText(true);
+    } else {
+      hideBlock(true);
+      hideText(false);
     }
   };
 
   const cbox = (id, e) => {
     if (e) {
       checkedIds.add(id);
-    } else {
-      if (!e && checkedIds.has(id)) {
-        checkedIds.delete(id);
-      }
     }
+    if (!e && checkedIds.has(id)) {
+      checkedIds.delete(id);
+    }
+    console.log(checkedIds)
   };
 
   return (
@@ -56,14 +59,19 @@ function App() {
       {list.map((item, index) => (
         <>
           <div>
-            <span  hidden={isHiddenTxt} key={"name" + index} id={"input" + item.id}>
-              {item.name} 
+            <span
+              hidden={isHiddenTxt}
+              key={"name" + index}
+              id={"input" + item.id}
+            >
+              {item.name}
             </span>
-             <input hidden={isHidden}
-                type="text"
-                name={"text" + index}
-                onChange={() => console.log(event.target.value)}
-              />
+            <input
+              hidden={isHidden}
+              type="text"
+              name={"text" + index}
+              onChange={() => console.log(event.target.value)}
+            />
 
             <div>
               <input
