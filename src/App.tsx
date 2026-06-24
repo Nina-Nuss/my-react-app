9a11537fd38c956c68450cde39453eb5c48f9fdf
Nina Nuss <maximnuss120697@gmail.com>
2026-06-24 19:39:39 +0200

    .

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,12 @@
 import "./App.css";
 import { useState } from "react";
-import { useRef } from "react";
 
 function App() {
-  const hiddenBlocks = useRef(0);
   const [isHidden, hideBlock] = useState(true);
   const [isHiddenTxt, hideText] = useState(false);
-  const newList = [];
   const checkedIds = new Set();
 
-  const [list, addVal] = useState([
+  let [list, addVal] = useState([
     {
       id: 1,
       name: "nina",
@@ -18,20 +15,19 @@ function App() {
   ]);
 
   const deleteList = () => {
-    
     const idsToDelete = new Set(checkedIds);
-    const newList = list.filter((item) => !idsToDelete.has(item.id));
+    list = list.filter((item) => !idsToDelete.has(item.id));
     // list.forEach((obj) => {
     //   if (idsToDelete.includes(obj.id)) {
     //     newList.push(obj);
     //   }
     // });
-    
 
-    console.log("checkedid: ", checkedIds)
-    addVal(newList);
-    console.log(newList);
-    console.log(list)
+    console.log("checkedid: ", checkedIds);
+    console.log("gefilterte liste: ", list);
+    addVal(list);
+    console.log("endliste: ", list);
+    // idsToDelete.clear()
   };
 
   const changeItem = (id) => {
@@ -45,13 +41,13 @@ function App() {
   };
 
   const cbox = (id, e) => {
-    if (e) {
+    if (e && !checkedIds.has(id)) {
       checkedIds.add(id);
     }
     if (!e && checkedIds.has(id)) {
       checkedIds.delete(id);
     }
-    console.log(checkedIds)
+    console.log(checkedIds);
   };
 
   return (
@@ -66,20 +62,16 @@ function App() {
             >
               {item.name}
             </span>
-            <input
-              hidden={isHidden}
-              type="text"
-              name={"text" + index}
-              onChange={() => console.log(event.target.value)}
-            />
+            <input hidden={isHidden} type="text" name={"text" + index} />
 
             <div>
               <input
                 type="checkbox"
                 name={"chbx" + index}
-                onClick={(event) => cbox(item.id, event.target.checked)}
+                
+                onChange={(event) => cbox(item.id, event.target.checked)}
               />
-              <button type="button" onClick={(event) => changeItem(item.id)}>
+              <button type="button" onClick={(e) => changeItem(item.id)}>
                 edit
               </button>
             </div>
